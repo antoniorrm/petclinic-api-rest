e4e6b15198eea1cfcc0457133629abb3b7e18cbd
Antonio Raimundo <antonioraimundo007@gmail.com>
2018-09-24 15:03:35 -0300

    Create setup.heroku.sh

---
+++ .circleci/setup.heroku.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+sudo mkdir -p /usr/local/lib /usr/local/bin
+sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+
+cat > ~/.netrc << EOF
+machine api.heroku.com
+login antonioraimundo007@gmail.com
+password 0e0e43b6-78b7-4141-ae9a-33542b56cd05
+EOF
